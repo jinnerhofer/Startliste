f0717c977b69ab03155d3b1bc5a0511b897f82f7
jinnerhofer <57176526+jinnerhofer@users.noreply.github.com>
2021-02-03 08:42:22 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Startliste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
