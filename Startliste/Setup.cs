e09759f407cc64245f867bb3391cc14e74852e36
jinnerhofer <57176526+jinnerhofer@users.noreply.github.com>
2021-02-05 11:21:08 +0100

    Program verändert

--- Startliste/Setup.cs
+++ Startliste/Setup.cs
@@ -6,6 +6,34 @@ namespace Startliste
 {
     public class Setup
     {
+        static void Main(string[] args)
+        {
+            //Gemeinsam mit Anna Streitberger gemacht
 
+            List<IStarter> Startliste = new List<IStarter>();
+
+             int Zahl = 1;
+
+            while (Zahl < 0)
+            {
+                var Startnummer = Zahl; 
+                Console.Write("Vorname: ");
+                var Vorname = Console.ReadLine();
+                Console.Write("Nachname: ");
+                var Nachname = Console.ReadLine();
+                Console.Write("Geschlecht: ");
+                var Geschlecht = Console.ReadLine();
+                Console.Write("Nation: ");
+                var Nation = Console.ReadLine();
+                Startnummer++;
+
+                foreach (Starter s in Startliste)
+                {
+                    Startliste.Add(new Starter());
+                    Console.WriteLine(s);
+                }
+            }
+            
+        }
     }
 }
